3fbd6a98fc740eda1c1bc6d587130a431b90c49e
LiamABailey <bailey.liam102@gmail.com>
2022-06-19 12:18:24 -0400

    Restructured patternbuilders to share more code; improved query ingestor

---
+++ api/patterngen/beatrequestutils.go
@@ -0,0 +1,64 @@
+package patterngen
+
+import ( 
+	"fmt"
+	"sort"
+	"strings"
+	"strconv"
+)
+
+const (
+	beatIdentifier string = "b"
+	eq string = "="
+)
+
+// split ?b0=ride,snare,kick,hihat&b1=... queries into 
+// component strings
+func splitBeatQuery(query map[string][]string) ([]string, error) {
+	var unsupportedIds []string
+	//for each segment, we load into a map[int]string for future ordering
+	parsedBeats := make(map[int]string)
+	for qkey, qval := range query {
+		// try to parse id of beat 
+		qkeyId := qkey[1:]
+		beatId, atoierr := strconv.Atoi(qkeyId)
+		if atoierr == nil {
+			if len(query[qkey]) != 1 {
+				return make([]string, 0), fmt.Errorf("Expect one value per param, saw %s for %s", qval, qkey)
+			}
+			parsedBeats[beatId] = qval[0]
+		} else {
+			unsupportedIds = append(unsupportedIds, qkeyId)
+		}
+	}
+	// if unsupported ids, stop and exit
+	if len(unsupportedIds) != 0 {
+		return make([]string, 0), fmt.Errorf("Beat IDs not supported: %s", unsupportedIds)
+	}
+	// else, sort keys
+	mKeys := make([]int, len(parsedBeats))
+	for k := range parsedBeats {
+		mKeys = append(mKeys, k)
+	}
+	sort.Ints(mKeys)
+	// sort the beat values using the key order, from least to greatest
+	sortedBeats := make([]string, len(mKeys))
+	for sk := range mKeys {
+		sortedBeats = append(sortedBeats, parsedBeats[sk])
+	}	
+	return sortedBeats, nil
+}
+
+// parse a string 
+func splitConvertPatternLists(pat string, sep string) ([]int, error) {
+	sepPat := strings.Split(pat, sep)
+	iPat := make([]int, len(sepPat))
+	var cerr error
+	for i, v := range sepPat {
+	  iPat[i], cerr = strconv.Atoi(v)
+	  if cerr != nil {
+		return nil, cerr
+	  }
+	}
+	return iPat, nil
+  }
--- api/patterngen/patternbuilders.go
+++ api/patterngen/patternbuilders.go
@@ -17,7 +17,6 @@ const (
   defaultPatterns string = "0"
   defaultBeats string = "4"
   beatComponents int = 4
-  beatIdentifier string = "b"
   innerSep string = "|"
   outerSep string = ","
 )
@@ -32,10 +31,10 @@ func getMeasure(ctx *gin.Context) {
   snare := ctx.DefaultQuery("snare", defaultPatterns)
   beats, _ := strconv.Atoi(ctx.DefaultQuery("beats", defaultBeats))
 
-  bassPatterns, berr := convertPatternLists(bass, outerSep)
-  hihatPatterns, hherr := convertPatternLists(hihat, outerSep)
-  ridePatterns, rerr := convertPatternLists(ride, outerSep)
-  snarePatterns, serr := convertPatternLists(snare, outerSep)
+  bassPatterns, berr := splitConvertPatternLists(bass, outerSep)
+  hihatPatterns, hherr := splitConvertPatternLists(hihat, outerSep)
+  ridePatterns, rerr := splitConvertPatternLists(ride, outerSep)
+  snarePatterns, serr := splitConvertPatternLists(snare, outerSep)
 
   if berr != nil || hherr != nil || rerr != nil || serr != nil {
     ctx.JSON(http.StatusBadRequest, gin.H{"error": "Received malformed pattern parameter"})
@@ -56,18 +55,10 @@ func getMeasure(ctx *gin.Context) {
 // Given the construction request of one or more beats,
 // produce a measure meeting the constraints
 func generateMeasureFromBeats(ctx *gin.Context) {
-  // looks for b# identifiers, starting with b0
-  ok := true
-  ix := 0
-  var measureDefinitions []string
-  for ok {
-    beatId := beatIdentifier + strconv.Itoa(ix)
-    var beatDef string
-    beatDef, ok = ctx.GetQuery(beatId)
-    if ok {
-      measureDefinitions = append(measureDefinitions, beatDef)
-    }
-    ix += 1
+  measureDefinitions, qerr := splitBeatQuery(ctx.Request.URL.Query())
+  if qerr != nil {
+    ctx.JSON(http.StatusBadRequest,
+        gin.H{"error": fmt.Sprintf("Unable to parse query: %s", qerr.Error())})
   }
   // generate a pattern from each beat definition
   parsedMeasDef := make([]patterns.Beat, len(measureDefinitions))
@@ -78,14 +69,13 @@ func generateMeasureFromBeats(ctx *gin.Context) {
       log.Println("Expected beat construction request to have ", beatComponents,
                   " components, received ", len(sepParts))
       ctx.JSON(http.StatusBadRequest,
-            gin.H{"error": fmt.Sprintf("Each beat must have %s components", beatComponents)})
-      return
+            gin.H{"error": fmt.Sprintf("Each beat must have %d components", beatComponents)})
     }
     // convert the requested compoent IDs to integers
     parsedBeatDefinition := make([][]int, beatComponents)
     for c := 0; c < beatComponents; c++ {
       var err error
-      parsedBeatDefinition[c], err = convertPatternLists(sepParts[c], innerSep)
+      parsedBeatDefinition[c], err = splitConvertPatternLists(sepParts[c], innerSep)
       if err != nil{
         log.Println("Encountered error trying to parse beat components: ", err)
         ctx.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Couldn't parse %s", sepParts[c])})
@@ -103,15 +93,4 @@ func generateMeasureFromBeats(ctx *gin.Context) {
   ctx.JSON(http.StatusOK, gin.H{"pattern": string(pattern)})
 }
 
-func convertPatternLists(pat string, sep string) ([]int, error) {
-  sepPat := strings.Split(pat, sep)
-  iPat := make([]int, len(sepPat))
-  var cerr error
-  for i, v := range sepPat {
-    iPat[i], cerr = strconv.Atoi(v)
-    if cerr != nil {
-      return nil, cerr
-    }
-  }
-  return iPat, nil
-}
+
