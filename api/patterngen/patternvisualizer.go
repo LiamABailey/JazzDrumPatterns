c52fc41cd1f703759f9f5bc96f7d9ea2f4fc45be
LiamABailey <bailey.liam102@gmail.com>
2022-06-19 12:18:51 -0400

    First pass of 'real' implementation of image ingestor

--- api/patterngen/patternvisualizer.go
+++ api/patterngen/patternvisualizer.go
@@ -15,15 +15,42 @@ import (
 //"image/png"
 //"image/draw"
 
+const (
+  querySep string = ","
+)
+
 
 // given a collection of beats (a measure),
 // produce an image
-//func getMeasureImage(ctx *gin.Context) {
-  // TODO: implement
-  // step 1: parse each beat in the request
-  // step 2: get images for each beat
-  // step 3: concatenate images
-//}
+func getMeasureImage(ctx *gin.Context) {
+  // get each individual beat: ride,snare,bass,hh
+  beatSegments, qerr := splitBeatQuery(ctx.Request.URL.Query())
+  if qerr != nil {
+    ctx.JSON(http.StatusBadRequest,
+        gin.H{"error": fmt.Sprintf("Unable to parse query: %s", qerr.Error())})
+  }
+  // for each segment
+  for _, bSeg := range beatSegments {
+    //split into ride/snare/bass/hh 
+    beatComponents, serr := splitConvertPatternLists(bSeg, querySep)
+    if serr != nil {
+      ctx.JSON(http.StatusBadRequest,
+        gin.H{"error": fmt.Sprintf("Unable to parse query segment %s: %s",bSeg, serr.Error())})
+    }
+    if len(beatComponents) != 4 {
+      ctx.JSON(http.StatusBadRequest,
+        gin.H{"error": fmt.Sprintf("Expected 4 segments: ride/snare/bass/hh, received: %s",bSeg)})
+    }
+    imRS, imRSerr := retrieveImage(beatComponents[0], beatComponents[1], beatimages.RideSnareImages)
+    imBHH, imBHHerr := retrieveImage(beatComponents[2], beatComponents[3], beatimages.KickHiHatImages)
+    if (imRSerr != nil) || (imBHHerr != nil){
+      ctx.JSON(http.StatusInternalServerError,
+        gin.H{"error": fmt.Sprintf("Unable to retrieve images. Errors: Ride/Snare: %s; Bass/HiHat: %s", imRSerr.Error(), imBHHerr.Error())})
+    }
+    ctx.JSON(http.StatusOK, gin.H{"Ride-Snare": imRS, "Bass-HiHat": imBHH})
+  }
+
+}
 
 // given a known ride/hihat/snare/kick pattern,
 // compose and return a single beat
@@ -42,7 +69,6 @@ func retrieveImage(rhythm1, rhythm2 int, dir embed.FS) (string, error) {
   }
   imb64 := base64.StdEncoding.EncodeToString(imbytes)
   return imb64, nil
-
 }
 
 func TestRetrieve(ctx *gin.Context) {
