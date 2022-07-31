9fb3502faf2b9a8ee3e17482e5bf0e33c2fc9904
LiamABailey <bailey.liam102@gmail.com>
2022-07-31 16:57:24 -0400

    Added defaults to index to support testing, cleaned up JS

--- web/script/getbeat.js
+++ web/script/getbeat.js
@@ -1,9 +1,20 @@
-function getimage(){
+function getImage(){
+    query = getImageQuery();
+    fetch(query).then((resp )=> resp.json()).then((respJSON) => console.log(respJSON));
+}
+
+function getImageQuery() {
     beatids = Array.from(document.querySelectorAll('[id^="beat"]'));
     beatids.sort();
-    console.log(beatids)
     values = [];
-    beatids.forEach(b => values.push(document.querySelector(b.id).value));
-    console.log(values);
-    
+    beatids.forEach(b => values.push(document.getElementById(b.id).value));
+    // remove empties
+    values.filter(v => v.length > 0)
+    // build the query string
+    base = "http://localhost:8050/GetMeasureImage?";
+    params = [];
+    values.forEach((v, i) => params.push(`b${i}=${v}`));
+    qparams = params.join('&');
+    query = base + qparams;
+    return query
 }
